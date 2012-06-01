7d93a11b6dbefa45cc7a5bc2e1a0a3f7271caf94
Bryan Hansen <bryguy1300@gmail.com>
2012-06-01 00:52:00 -0700

    Adding stubs for LOVGridView.

---
+++ Layover/Layover/LOVGridView.h
@@ -0,0 +1,13 @@
+//
+//  LOVGridView.h
+//  Layover
+//
+//  Created by Bryan Hansen on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOVGridView : UIView
+
+@end
---
+++ Layover/Layover/LOVGridView.m
@@ -0,0 +1,31 @@
+//
+//  LOVGridView.m
+//  Layover
+//
+//  Created by Bryan Hansen on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LOVGridView.h"
+
+@implementation LOVGridView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
